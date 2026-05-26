dabfeb377aa818a4eb38b718e99904fc186643c1
Joshua8600 <105813016+Joshua8600@users.noreply.github.com>
2026-05-26 11:31:21 -0700

    Exercise 07 Completed

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let arr = str.split('');
+    arr.reverse();
+    return arr.join('');
 };
+
+console.log(reverseString('hello there'));
 
 // Do not edit below this line
 module.exports = reverseString;
