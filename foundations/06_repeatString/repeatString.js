09713b933f708ed5fc0e1042154327a6334d64ab
Joshua8600 <105813016+Joshua8600@users.noreply.github.com>
2026-05-26 11:19:23 -0700

    Exercise 06 completed

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+function repeatString(str, num) {
+    let output = '';
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 1; i <= num; i++) {
+            output += str;
+        }
+    } return output
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
