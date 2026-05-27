058022aeb1a4e3b457266d3d5d2f70515e59073a
Joshua8600 <105813016+Joshua8600@users.noreply.github.com>
2026-05-26 23:53:38 -0700

    Exercise 09 Completed

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let toAdd=[]
+    if (Number.isInteger(a) == true && Number.isInteger(b) == true ) {
+        if (a > 0 && b > 0 && b > a) {
+            for (let i = a; i <= b; i++ ) {
+                toAdd.push(i);
+            };
+        let sum = toAdd.reduce((sum, current) => sum + current, 0);
+        return sum;
+      } else if (a > 0 && b > 0 && b < a) {
+            for (let i = b; i <= a; i++ ) {
+                toAdd.push(i);
+            };
+        let sum = toAdd.reduce((sum, current) => sum + current, 0);
+        return sum;
+      } else if (a < 0 || b < 0) {
+            return "ERROR";
+        };
+    } else {
+        return "ERROR";
+    };
 };
 
 // Do not edit below this line
