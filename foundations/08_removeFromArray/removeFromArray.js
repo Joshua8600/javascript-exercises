8b04c9d7ef2473d3bcc560b2b2c74c179dfc7f01
Joshua8600 <105813016+Joshua8600@users.noreply.github.com>
2026-05-26 23:11:56 -0700

    Exercise 08 Completed

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...values) {
+    let checkArray = Array.from(values);
+    let newArray = []
+    for (const item of array) {
+        if (!checkArray.includes(item)) {
+            newArray.push(item);
+        }
+    }
+    return newArray;
+}
+
+let array = [1, 2, 3, 4, 5]
+console.log (removeFromArray(array, 3, 4, 5));
 
 // Do not edit below this line
 module.exports = removeFromArray;
